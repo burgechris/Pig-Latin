9c02091e69f3084bde0d8ac99db491be7a1399d6
Christopher Burge and micah zaayer <micah@>
2019-08-21 15:37:53 -0700

    add scripts to the js file to determine the vowel location in a string

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
-var vowels = ["a", "e", "i", "o", "u"];
-var con = ["q", "w", "r", "t", "y", "p", "s", "d", "f", "g", "h", "j", "k", "l",
-"z", "x", "c", "v", "b", "n", "m"];
-var translator = function(characterArray) {
-  for (var i = 0; i < characterArray.length; i++) {
-    if ((vowels.includes(characterArray[i])) || con.includes(characterArray[i])) {
-      console.log("Please enter a real word.")
-    }
-      else if (i === 0 && i === vowels) {
-      console.log(characterArray + "yay");
-    }
-
-
-  }
-}
 
 
 $(document).ready(function(){
   $('.translator').submit(function(event){
-  event.preventDefault();
-  var word = $('input#sentence').val();
-  var characterArray = word.split('');
-  var wordArray = word.split(" ");
-  console.log(characterArray);
-  console.log(wordArray);
-  var result = translator(wordArray);
+    event.preventDefault();
+    var userInput = $('#userInput').val();
+    console.log(userInput);
+    var vowels = ["a", "e", "i", "o", "u"];
+
+    if (vowels.includes(userInput.charAt(0))) {
+      console.log(userInput.concat('yay'));
+    }  else {
+      var indexOfFirstVowel
+      var frontCons
+      var result
+      for (var i = 0; i < userInput.length; i++) {
+        if (vowels.includes(userInput[i])) {
+          indexOfFirstVowel = i;
+          frontCons = userInput.slice(0, i);
+          result = userInput.slice(i, userInput.length)
+          console.log(result.concat(frontCons + "yay"));
+        }
+      }
+
+    }
+
+
+
   });
 });
