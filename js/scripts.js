3746e580ce9b09816bd00088a31c40f6b5f8bc00
Christopher Burge and micah zaayer <micah@>
2019-08-21 12:01:36 -0700

    add if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
-var translator = function(phraseArray) {
-  if ()
+var vowels = ["a", "e", "i", "o", "u"];
+var con = ["q", "w", "r", "t", "y", "p", "s", "d", "f", "g", "h", "j", "k", "l",
+"z", "x", "c", "v", "b", "n", "m"];
+var translator = function(characterArray) {
+  for (var i = 0; i < characterArray.length; i++) {
+    if ((vowels.includes(characterArray[i])) || con.includes(characterArray[i])) {
+      console.log("Please enter a real word.")
+    }
+      else if (i === 0 && i === vowels) {
+      console.log(characterArray + "yay");
+    }
+
+
+  }
 }
 
 
 $(document).ready(function(){
   $('.translator').submit(function(event){
   event.preventDefault();
-  var phrase = $('input#sentence').val();
-  var phraseArray = phrase.split('');
-  console.log(phraseArray);
-
+  var word = $('input#sentence').val();
+  var characterArray = word.split('');
+  var wordArray = word.split(" ");
+  console.log(characterArray);
+  console.log(wordArray);
+  var result = translator(wordArray);
   });
 });
