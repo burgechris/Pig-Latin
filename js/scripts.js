23d0d16db05fbe73cbba64b0b5e3974ac71db502
Christopher Burge and micah zaayer <christopher@>
2019-08-21 16:12:20 -0700

    add functioning loops to translate single words into pig latin.

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,23 @@ $(document).ready(function(){
       var frontCons
       var result
       for (var i = 0; i < userInput.length; i++) {
-        if (vowels.includes(userInput[i])) {
+        if (userInput[0] === "q" && userInput[1] ==="u") {
+          frontCons = userInput.slice(0, 2);
+          result = userInput.slice(2, userInput.length);
+          console.log(result.concat(frontCons + "ay"));
+        }
+        else if (userInput[1] === "q" && userInput[2] ==="u") {
+          frontCons = userInput.slice(0, 3);
+          result = userInput.slice(3, userInput.length);
+          console.log(result.concat(frontCons + "ay"));
+
+        }
+        else if (vowels.includes(userInput[i])) {
           indexOfFirstVowel = i;
           frontCons = userInput.slice(0, i);
-          result = userInput.slice(i, userInput.length)
-          console.log(result.concat(frontCons + "yay"));
+          result = userInput.slice(i, userInput.length);
+          console.log(result.concat(frontCons + "ay"));
+          break;
         }
       }
 
